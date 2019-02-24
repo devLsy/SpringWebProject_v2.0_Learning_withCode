d2eb25db7e2e206d186460c6118be1bb945cb942
devLsy <infightersy@naver.com>
2019-02-24 20:12:52 +0900

    20190224 20:12 페이지네이션 구현 중....

---
+++ ex02/sql/BOOK.sql
@@ -0,0 +1,18 @@
+SELECT /*+ INDEX_DESC(tbl_board pk_board) */
+    rownum rn, bno, title, content 
+FROM tbl_board
+    WHERE rownum <= 20;
+    
+
+-- 인라인 뷰 적용
+SELECT
+    bno, title, content
+FROM 
+    (
+    SELECT /*+ INDEX_DESC(tbl_board pk_board)*/
+        rownum rn, bno, title, content
+    FROM 
+        tbl_board
+    WHERE rownum <= 20
+    )
+WHERE rn > 10;
--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -3,11 +3,14 @@ package org.zerock.mapper;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardMapper {
 	
 	/*@Select("SELECT * FROM tbl_board WHERE bno > 0")*/
 	public List<BoardVO> getList();
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
 	
 	public void insert(BoardVO board);
 	
@@ -18,4 +21,6 @@ public interface BoardMapper {
 	public int delete(Long bno);
 	
 	public int update(BoardVO board);
+	
+	
 }
