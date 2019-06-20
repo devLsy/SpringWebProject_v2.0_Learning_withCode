d7bb0e504c4b5d79b9c0c79dae373db8f91addd9
LeeSangYeop <44331989+devLsy@users.noreply.github.com>
2019-06-20 10:07:11 +0900

    20190620 10:05 서버의 파일 업로드 경로에서 어제날짜의 파일목록을 DB의 파일 목록과 비교해서 DB에 없는 파일 목록 삭제 로직 추가(스케줄러)

--- ex05/src/main/java/org/zerock/task/FileCheckTask.java
+++ ex05/src/main/java/org/zerock/task/FileCheckTask.java
@@ -1,11 +1,18 @@
 package org.zerock.task;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.zerock.domain.BoardAttachVO;
 import org.zerock.mapper.BoardAttachMapper;
 
 import lombok.Setter;
@@ -27,7 +34,44 @@ public class FileCheckTask {
 		log.info("cal: " + cal);
 		
 		String str = sdf.format(cal.getTime());
+		log.info("str: " + str);
 		return str.replace("-", File.separator);
+	}
+	
+	@Scheduled(cron = "5 * * * * * ")	
+	public void checkFiles() throws Exception {
+		log.warn("File Check Task run~!!");
+		log.warn(new Date());
+		
+		List<BoardAttachVO> fileList = attachMapper.getOldFiles();
+		
+		List<Path> fileListPaths = fileList.stream()
+				.map(vo -> Paths.get("C:\\dev\\private\\upload\\tmp", vo.getUploadPath(), vo.getUuid() + "_" + vo.getFileName()))
+				.collect(Collectors.toList());
+		log.info("fileListPaths: " + fileListPaths);
+		
+		fileList.stream().filter(vo -> vo.isFileType() == true)
+		.map(vo -> Paths.get("C:\\dev\\private\\upload\\tmp", vo.getUploadPath(), "s_" + vo.getUuid() + "_" + vo.getFileName()))
+		.forEach(p -> fileListPaths.add(p));
+		
+		log.warn("===========================================");
+		
+		fileListPaths.forEach(p -> log.warn(p));
+			
+		File targetDir = Paths.get("C:\\dev\\private\\upload\\tmp", getFolderYesterDay()).toFile();
+		log.info("targetDri: " + targetDir);
+		
+		File[] removeFiles = targetDir.listFiles(file -> fileListPaths.contains(file.toPath()) == false);
+		
+		log.warn("---------------------------------------------------------------------------------");
+		for(File file : removeFiles) {
+				log.warn(file.getAbsolutePath());
+				file.delete();
+		}
+		
+		
+		
+		
 	}
 	
 	
